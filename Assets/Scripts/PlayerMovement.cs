41fb0ad6644f9c87ff47e4bcf7c477220f863894
jacob-sahl <jacobsahlmueller@gmail.com>
2022-09-30 13:29:44 -0400

    new input system movement and camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,126 +6,112 @@ using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("References")] [Tooltip("Reference to the main camera used for the player")]
-    public Camera playerCamera;
+  [Header("References")]
+  [Tooltip("Reference to the main camera used for the player")]
+  public Camera playerCamera;
 
-    [Header("Rotation")] [Tooltip("Rotation speed for moving the camera")]
-    public float RotationSpeed = 200f;
+  [Header("Rotation")]
+  [Tooltip("Rotation speed for moving the camera")]
+  public float RotationSpeed = 2f;
 
-    [Header("Movement")] [Tooltip("Max movement speed")]
-    public float maxSpeed = 10f;
+  [Header("Movement")]
+  [Tooltip("Max movement speed")]
+  public float maxSpeed = 10f;
 
-    [Tooltip(
-        "Sharpness for the movement when grounded, a low value will make the player accelerate and decelerate slowly, a high value will do the opposite")]
-    public float MovementSharpness = 15;
+  [Tooltip(
+      "Sharpness for the movement when grounded, a low value will make the player accelerate and decelerate slowly, a high value will do the opposite")]
+  public float MovementSharpness = 15;
+  public Vector3 CharacterVelocity;
+  PlayerInputHandler _inputHandler;
+  Vector2 cameraRotation = new Vector2(0, 0);
+  CharacterController _controller;
+  Outline _lastOutline;
 
-    public Vector3 CharacterVelocity;
+  // Start is called before the first frame update
+  void Start()
+  {
+    _controller = GetComponent<CharacterController>();
+    _inputHandler = GetComponent<PlayerInputHandler>();
+  }
 
-    PlayerInputHandler _inputHandler;
-    float _cameraVerticalAngle = 0f;
-    
-    CharacterController _controller;
-    
-    Outline _lastOutline;
-
-    // Start is called before the first frame update
-    void Start()
+  void HandleCharacterMovement()
+  {
+    // Handle looking
     {
-        _controller = GetComponent<CharacterController>();
-        _inputHandler = GetComponent<PlayerInputHandler>();
+      Vector2 lookInput = _inputHandler.GetLookInput();
+      // Process the raw lookInput 
+      Vector2 lookProcessed = lookInput * RotationSpeed * Time.deltaTime;
+      cameraRotation.y -= lookProcessed.y;
+      // Clamp y-rotation to a range of 180 degrees
+      cameraRotation.y = Mathf.Clamp(cameraRotation.y, -89f, 89f);
+      cameraRotation.x += lookProcessed.x;
+      // Apply only x rotation to player
+      transform.localRotation = Quaternion.Euler(0f, cameraRotation.x, 0f);
+      // Apply both x and y rotation to camera
+      playerCamera.transform.rotation = Quaternion.Euler(cameraRotation.y, cameraRotation.x, 0f);
     }
 
-    void HandleCharacterMovement()
+    // Handle movement
     {
-        // horizontal character rotation
+      Vector2 moveInput = _inputHandler.GetMoveInput();
+      Vector3 movement = (moveInput.y * transform.forward) + (moveInput.x * transform.right);
+      _controller.Move(movement * maxSpeed * Time.deltaTime);
+    }
+  }
+
+
+  private void OnFire()
+  {
+    //Debug.Log("Mouse Clicked");
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    RaycastHit hit;
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    if (Physics.Raycast(ray, out hit))
+    {
+      var colliderGameObject = hit.collider.gameObject;
+      var outline = colliderGameObject.GetComponent<Outline>();
+      if (outline != null)
+      {
+        if (_lastOutline != null && _lastOutline != outline)
         {
-            // rotate the transform with the input speed around its local Y axis
-            transform.Rotate(
-                new Vector3(0f, (_inputHandler.GetLookInputsHorizontal() * RotationSpeed),
-                    0f), Space.Self);
+          _lastOutline.enabled = false;
+        }
+        _lastOutline = outline;
+        _lastOutline.OutlineMode = Outline.Mode.OutlineAll;
+        _lastOutline.OutlineWidth = 5;
+
+        if (_inputHandler.GetInteractInputDown())
+        {
+          InteractEvent interact = Events.InteractEvent;
+          interact.ObjectTag = hit.collider.gameObject.tag;
+          EventManager.Broadcast(interact);
         }
 
-        // vertical camera rotation
+        if (_inputHandler.GetInteractInputHeld())
         {
-            // add vertical inputs to the camera's vertical angle
-            _cameraVerticalAngle += _inputHandler.GetLookInputsVertical() * RotationSpeed;
-
-            // limit the camera's vertical angle to min/max
-            _cameraVerticalAngle = Mathf.Clamp(_cameraVerticalAngle, -89f, 89f);
-
-            // apply the vertical angle as a local rotation to the camera transform along its right axis (makes it pivot up and down)
-            playerCamera.transform.localEulerAngles = new Vector3(_cameraVerticalAngle, 0, 0);
+          _lastOutline.OutlineWidth = 5;
+          _lastOutline.OutlineColor = Color.red;
         }
-
-        // character movement handling
+        else
         {
-            // converts move input to a worldspace vector based on our character's transform orientation
-            Vector3 worldspaceMoveInput = transform.TransformVector(_inputHandler.GetMoveInput());
-
-            // calculate the desired velocity from inputs, max speed
-            Vector3 targetVelocity = worldspaceMoveInput * maxSpeed;
-
-            // smoothly interpolate between our current velocity and the target velocity based on acceleration speed
-            CharacterVelocity = Vector3.Lerp(CharacterVelocity, targetVelocity,
-                MovementSharpness * Time.deltaTime);
+          _lastOutline.OutlineColor = Color.white;
         }
-
-        _controller.Move(CharacterVelocity * Time.deltaTime);
+        _lastOutline.enabled = true;
+      }
+      else
+      {
+        if (_lastOutline != null)
+        {
+          _lastOutline.enabled = false;
+        }
+      }
     }
 
+    HandleCharacterMovement();
 
-    private void OnFire()
-    {
-        //Debug.Log("Mouse Clicked");
-    }
-    
-// Update is called once per frame
-    void Update()
-    {
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit))
-        {
-            var colliderGameObject = hit.collider.gameObject;
-            var outline = colliderGameObject.GetComponent<Outline>();
-            if (outline != null)
-            {
-                if (_lastOutline != null && _lastOutline != outline)
-                {
-                    _lastOutline.enabled = false;
-                }
-                _lastOutline = outline;
-                _lastOutline.OutlineMode = Outline.Mode.OutlineAll;
-                _lastOutline.OutlineWidth = 5;
-
-                if (_inputHandler.GetInteractInputDown())
-                {
-                    InteractEvent interact = Events.InteractEvent;
-                    interact.ObjectTag = hit.collider.gameObject.tag;
-                    EventManager.Broadcast(interact);
-                }
-                
-                if (_inputHandler.GetInteractInputHeld())
-                {
-                    _lastOutline.OutlineWidth = 5;
-                    _lastOutline.OutlineColor = Color.red;
-                }
-                else
-                {
-                    _lastOutline.OutlineColor = Color.white;
-                }
-                _lastOutline.enabled = true;
-            }
-            else
-            {
-                if (_lastOutline != null)
-                {
-                    _lastOutline.enabled = false;
-                }
-            }
-        }
-        
-        HandleCharacterMovement();
-
-    }
+  }
 }
