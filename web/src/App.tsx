a2bf96fb42bc9c360915edc40776bc34abd63016
Tongkun Zhang <kennardzhang@gmail.com>
2022-10-18 15:50:43 -0400

    Added card page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import bg from "./assets/images/bg.jpg";
 import Card from "./components/Card";
 
 function App() {
     return (
         <div className="App">
+            <div
+                className="App-background"
+                // style={{ backgroundImage: `url(${bg})` }}
+            ></div>
             <header className="App-header">
-                <h1 className="text-3xl font-bold font-[XTypewriter]">
+                <h1 className="text-3xl font-bold font-[XTypewriter] text-white select-none">
                     Red Herring!
                 </h1>
             </header>
