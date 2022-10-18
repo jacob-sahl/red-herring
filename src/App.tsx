02f138196b3d35a21a9886ade47780e62538dca7
Tongkun Zhang <kennardzhang@gmail.com>
2022-10-18 11:44:43 -0400

    Added place holder

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1 className="text-3xl font-bold underline bg-violet-500 hover:bg-violet-600">
-                    Hello world!
+                <h1 className="text-3xl font-bold font-[XTypewriter]">
+                    Red Herring!
                 </h1>
             </header>
         </div>
