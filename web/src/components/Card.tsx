70d093afdc4306c61d4c216ba6974e81406f1c07
Tongkun Zhang <kennardzhang@gmail.com>
2022-10-22 22:19:58 -0400

    Added card tooltips

--- web/src/components/Card.tsx
+++ web/src/components/Card.tsx
@@ -28,20 +28,35 @@ function Card() {
     return (
         <div className="card-wrapper m-auto aspect-[10/16]">
             <div
-                className="card m-auto cursor-pointer select-none hover:drop-shadow-2xl transition-shadow"
+                className="card m-auto select-none hover:drop-shadow-2xl transition-shadow"
                 style={{ backgroundImage: `url(${getBackground(cardInfo)})` }}
             >
-                <div className="card-content card-clue ">
+                <div className="card-content">
                     <p>Clue:</p>
                     <p>{cardInfo.c}</p>
+                    <span className="tooltip-below">
+                        This will help you solve the typewriter puzzle. You
+                        choose when, how, and whether or not you want to reveal
+                        it to the detective. You can also lie about your clue in
+                        order to complete your secret objective
+                    </span>
                 </div>
                 <img
                     className="card-foreground"
                     src={getForeground(cardInfo)}
                 />
-                <div className="card-content card-objective">
+                <div className="card-content">
                     <p>Secret Objective:</p>
                     <p>{cardInfo.o}</p>
+                    <span className="tooltip-above">
+                        This will not help you solve the typewriter puzzle, but
+                        if you can manipulate the detective into completing this
+                        task, you&apos;ll get more points for this round and
+                        everyone else will get fewer!
+                        <br />
+                        Remember: no one gets any points unless the puzzle is
+                        solved!
+                    </span>
                 </div>
             </div>
         </div>
