4596b7c1c86c1016c51c6701afad1ae47d067449
jacob-sahl <jacobsahlmueller@gmail.com>
2022-11-08 01:19:48 -0500

    text reveal improvement

--- Assets/Scripts/MainMenu/TextReveal.cs
+++ Assets/Scripts/MainMenu/TextReveal.cs
@@ -11,6 +11,7 @@ public class TextReveal : MonoBehaviour
   float time;
   TextMeshProUGUI mesh;
   string fullText;
+  string[] fullTextWords;
   void Awake()
   {
     animating = false;
@@ -21,7 +22,9 @@ public class TextReveal : MonoBehaviour
   {
     mesh = GetComponent<TextMeshProUGUI>();
     fullText = mesh.text;
-    mesh.text = "";
+    fullTextWords = mesh.text.Split(' ');
+    // Hide the text to start
+    mesh.text = "<color=#00000000>" + fullText + "</color>";
   }
 
   private void OnDestroy()
@@ -53,7 +56,85 @@ public class TextReveal : MonoBehaviour
       time += Time.deltaTime;
       float proportion = time / duration;
       int charsToReveal = Mathf.Min(Mathf.RoundToInt(fullText.Length * proportion), fullText.Length);
-      mesh.text = fullText.Substring(0, charsToReveal);
+      string tempText = "<color=#FFFFFFFF>";
+      int index = 0;
+      while (index < charsToReveal)
+      {
+        tempText += fullText[index];
+        index++;
+      }
+      tempText += "</color>";
+      if (index < fullText.Length)
+      {
+        tempText += "<color=#FFFFFFBF>";
+        tempText += fullText[index];
+        tempText += "</color>";
+        index++;
+      }
+      if (index < fullText.Length)
+      {
+        tempText += "<color=#FFFFFF7F>";
+        tempText += fullText[index];
+        tempText += "</color>";
+        index++;
+      }
+      if (index < fullText.Length)
+      {
+        tempText += "<color=#FFFFFF3F>";
+        tempText += fullText[index];
+        tempText += "</color>";
+        index++;
+      }
+      tempText += "<color=#FFFFFF00>";
+      while (index < fullText.Length)
+      {
+        tempText += fullText[index];
+        index++;
+      }
+      tempText += "</color>";
+      mesh.text = tempText;
+
+      // For word-by-word reveal:
+
+      // int wordsToReveal = Mathf.Min(Mathf.RoundToInt(fullTextWords.Length * proportion), fullTextWords.Length);
+      // string tempText = "<color=#FFFFFFFF>";
+      // int wordIndex = 0;
+      // while (wordIndex < wordsToReveal)
+      // {
+      //   tempText += fullTextWords[wordIndex] + ' ';
+      //   wordIndex++;
+      // }
+      // tempText += "</color>";
+      // if (wordIndex < fullTextWords.Length)
+      // {
+      //   tempText += "<color=#FFFFFFBF>";
+      //   tempText += fullTextWords[wordIndex] + ' ';
+      //   tempText += "</color>";
+      //   wordIndex++;
+      // }
+      // if (wordIndex < fullTextWords.Length)
+      // {
+      //   tempText += "<color=#FFFFFF7F>";
+      //   tempText += fullTextWords[wordIndex] + ' ';
+      //   tempText += "</color>";
+      //   wordIndex++;
+      // }
+      // if (wordIndex < fullTextWords.Length)
+      // {
+      //   tempText += "<color=#FFFFFF3F>";
+      //   tempText += fullTextWords[wordIndex] + ' ';
+      //   tempText += "</color>";
+      //   wordIndex++;
+      // }
+      // tempText += "<color=#FFFFFF00>";
+      // while (wordIndex < fullTextWords.Length)
+      // {
+      //   tempText += fullTextWords[wordIndex] + ' ';
+      //   wordIndex++;
+      // }
+      // tempText += "</color>";
+      // mesh.text = tempText;
+
       if (time >= duration)
       {
         endAnimation();
