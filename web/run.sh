02d20526e2a4634e18b15a4ffefcf7add1da3b52
Tongkun Zhang <zhang@tongkun.io>
2022-11-10 23:15:36 -0500

    updated dockerfile

--- web/run.sh
+++ web/run.sh
@@ -1,2 +1,9 @@
-docker build -t red-herring-docker .
-docker run -p 3000:3000 red-herring-docker
+# Create a network, which allows containers to communicate
+# with each other, by using their container name as a hostname
+docker network create red_herring_network
+
+# Build prod using new BuildKit engine
+docker compose -f docker-compose.yml build
+
+# Up prod in detached mode
+docker compose -f docker-compose.yml up -d
