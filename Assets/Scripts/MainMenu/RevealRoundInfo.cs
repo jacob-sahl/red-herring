dc397e9102bce662bb499236ec9da315fc6e8224
Tongkun Zhang <kennardzhang@gmail.com>
2022-11-13 11:03:30 -0500

    formatting

--- Assets/Scripts/MainMenu/RevealRoundInfo.cs
+++ Assets/Scripts/MainMenu/RevealRoundInfo.cs
@@ -1,58 +1,55 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+
 namespace MainMenu
 {
-  public class RevealRoundInfo : MonoBehaviour
-  {
-    public List<GameObject> players = new List<GameObject>();
+    public class RevealRoundInfo : MonoBehaviour
+    {
+        public List<GameObject> players = new();
 
-    void Start()
-    {
-      UpdateAllPlayers();
-    }
-
-    private void UpdateAllPlayers()
-    {
-      for (int i = 0; i < PlayerManager.Instance.players.Count; i++)
-      {
-        UpdatePlayer(i);
-      }
-    }
-    void OnEnable()
-    {
-      UpdateAllPlayers();
-    }
-
-    private void UpdatePlayer(int playerID)
-    {
-      if (playerID < players.Count)
-      {
-        GameObject player = players[playerID];
-        var avatar = player.transform.Find("Avatar");
-        PlayerController playerController = PlayerManager.Instance.getPlayer(playerID);
-        avatar.GetComponent<Image>().color = playerController.color;
-        Debug.Log("Player: " + playerID + " Role: " + playerController.role);
-        if (playerController.role == PlayerRole.Detective)
+        private void Start()
         {
-          player.transform.Find("Role").GetComponent<TMP_Text>().text = "Detective";
-          var qrCode = player.transform.Find("QRCode").gameObject;
-          qrCode.SetActive(false);
+            UpdateAllPlayers();
         }
-        else
+
+        private void OnEnable()
         {
-          SecretObjective secret = GameController.Instance.getPlayersSecretObjective(playerID);
-          player.transform.Find("Role").GetComponent<TMP_Text>().text = "Informant";
-          var qrCode = player.transform.Find("QRCode").gameObject;
-          qrCode.SetActive(true);
-          qrCode.GetComponent<QRCodeObject>().QRCodeContent = CardURLGenerator.GetCardURL("1", "window",
-              secret.clue, secret.description);
-          Debug.Log("Secret: " + secret.description);
+            UpdateAllPlayers();
         }
-      }
+
+        private void UpdateAllPlayers()
+        {
+            for (var i = 0; i < PlayerManager.Instance.players.Count; i++) UpdatePlayer(i);
+        }
+
+        private void UpdatePlayer(int playerID)
+        {
+            if (playerID < players.Count)
+            {
+                var player = players[playerID];
+                var avatar = player.transform.Find("Avatar");
+                var playerController = PlayerManager.Instance.getPlayer(playerID);
+                avatar.GetComponent<Image>().color = playerController.color;
+                Debug.Log("Player: " + playerID + " Role: " + playerController.role);
+                if (playerController.role == PlayerRole.Detective)
+                {
+                    player.transform.Find("Role").GetComponent<TMP_Text>().text = "Detective";
+                    var qrCode = player.transform.Find("QRCode").gameObject;
+                    qrCode.SetActive(false);
+                }
+                else
+                {
+                    var secret = GameController.Instance.getPlayersSecretObjective(playerID);
+                    player.transform.Find("Role").GetComponent<TMP_Text>().text = "Informant";
+                    var qrCode = player.transform.Find("QRCode").gameObject;
+                    qrCode.SetActive(true);
+                    qrCode.GetComponent<QRCodeObject>().QRCodeContent = CardURLGenerator.GetCardURL("1", "window",
+                        secret.clue, secret.description);
+                    Debug.Log("Secret: " + secret.description);
+                }
+            }
+        }
     }
-  }
 }
