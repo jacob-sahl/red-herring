e26e6fd830783e0264f1b6781fed41dc02ef7826
jacob-sahl <jacobsahlmueller@gmail.com>
2022-12-03 14:10:17 -0500

    new secret objectives

--- Assets/Scripts/Puzzles/Puzzle.cs
+++ Assets/Scripts/Puzzles/Puzzle.cs
@@ -2,27 +2,27 @@
 
 public abstract class Puzzle : MonoBehaviour
 {
-    public delegate void completeCallback();
+  public delegate void completeCallback();
 
-    public string puzzleName;
-    public LevelManager levelManager;
-    public bool isComplete;
-    private completeCallback _complete_callback;
+  public string puzzleName;
+  public LevelManager levelManager;
+  public bool isComplete;
+  private completeCallback _complete_callback;
 
-    public virtual void Awake()
-    {
-        levelManager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
-        levelManager.addPuzzle(this);
-    }
+  public virtual void Awake()
+  {
+    levelManager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+    levelManager.addPuzzle(this);
+  }
 
-    public void SetCompleteCallback(completeCallback callback)
-    {
-        _complete_callback = callback;
-    }
+  public void SetCompleteCallback(completeCallback callback)
+  {
+    _complete_callback = callback;
+  }
 
-    public void Complete()
-    {
-        isComplete = true;
-        if (_complete_callback != null) _complete_callback();
-    }
+  public void Complete()
+  {
+    isComplete = true;
+    if (_complete_callback != null) _complete_callback();
+  }
 }
